25b3bd0324c5a104cbdd7e2ddacdc6ce5b51eb17
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-09-23 21:46:15 +0100

    Add dotenv configuration to rl-micro-payments
    Included dotenv dependency and configuration to handle environment variables based on the NODE_ENV. Modified protoPath handling to be dynamic according to the environment for better flexibility in production and development settings. Added esModuleInterop setting in tsconfig.json to enable seamless imports.

--- web/backend/rl-microservices/apps/rl-micro-payments/src/main.ts
+++ web/backend/rl-microservices/apps/rl-micro-payments/src/main.ts
@@ -4,6 +4,16 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { PAYMENTS_PACKAGE_NAME } from '../../../proto/payments';
 import { join } from 'path';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+function getProtoPath() {
+  return process.env.NODE_ENV === 'production'
+    ? 'dist/apps/proto/payments.proto'
+    : join(__dirname, '../proto/payments.proto');
+}
+
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     RlMicroPaymentsModule,
@@ -11,7 +21,7 @@ async function bootstrap() {
       transport: Transport.GRPC,
       options: {
         package: PAYMENTS_PACKAGE_NAME,
-        protoPath: join(__dirname, '../proto/payments.proto'),
+        protoPath: getProtoPath(),
       },
     },
   );
