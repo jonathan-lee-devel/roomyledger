a2d20c034b2485efffc0717b40b9d03a20381fa2
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-09-23 22:03:28 +0100

    Update production proto path in config.utils.ts
    Changed the base path for proto files in production from 'dist/apps/proto' to '/app/dist/apps/proto'. This alteration aligns with the new directory structure in the production environment.

--- web/backend/rl-microservices/libs/config/src/config.utils.ts
+++ web/backend/rl-microservices/libs/config/src/config.utils.ts
@@ -2,6 +2,6 @@ import { join } from 'path';
 
 export const getProtoPath = (protoFile: string) => {
   return process.env.NODE_ENV === 'production'
-    ? `dist/apps/proto/${protoFile}`
+    ? `/app/dist/apps/proto/${protoFile}`
     : join(__dirname, `../proto/${protoFile}`);
 };
