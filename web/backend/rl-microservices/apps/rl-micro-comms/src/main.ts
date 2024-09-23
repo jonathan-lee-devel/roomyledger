9cbd032da5617976901cde9f8edcc59443dfd59b
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-09-23 23:40:52 +0100

    Add rl-micro-comms microservice with gRPC support
    Update build and start scripts to include rl-micro-comms. Change the rl-micro-comms main module to initialize as a gRPC microservice, specifying transport options and proto file path.

--- web/backend/rl-microservices/apps/rl-micro-comms/src/main.ts
+++ web/backend/rl-microservices/apps/rl-micro-comms/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { RlMicroCommsModule } from './rl-micro-comms.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { PAYMENTS_PACKAGE_NAME } from '../../../proto/payments';
+import { getProtoPath } from '@rl-config/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(RlMicroCommsModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    RlMicroCommsModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: `0.0.0.0:10001`,
+        package: PAYMENTS_PACKAGE_NAME,
+        protoPath: getProtoPath('payments.proto'),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
