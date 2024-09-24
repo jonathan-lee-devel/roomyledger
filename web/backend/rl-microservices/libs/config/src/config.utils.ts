49bbce66a17e9ea81b962c16bbdb2e43cdcf8625
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-09-24 01:36:15 +0100

    Remove environment-based path selection logic.
    Simplified the `getProtoPath` function to always use `join(__dirname, '../proto/${protoFile}')`, eliminating the conditional logic based on the `NODE_ENV` environment variable. This change ensures a consistent behavior regardless of the environment.

--- web/backend/rl-microservices/libs/config/src/config.utils.ts
+++ web/backend/rl-microservices/libs/config/src/config.utils.ts
@@ -1,8 +1,5 @@
 import { join } from 'path';
 
 export const getProtoPath = (protoFile: string) => {
-  return process.env.NODE_ENV === 'production' ||
-    process.env.NODE_ENV === 'staging'
-    ? `/app/proto/${protoFile}`
-    : join(__dirname, `../proto/${protoFile}`);
+  return join(__dirname, `../proto/${protoFile}`);
 };
