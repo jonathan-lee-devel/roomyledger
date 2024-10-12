57415a646c354821b2188f9b1178c4d72147b96b
Jonathan Lee <43010048+jonathan-lee-devel@users.noreply.github.com>
2024-10-12 23:46:58 +0100

    Cleanup project files (#26)
    Deleted various configuration, image, font, SCSS, and Astro files that were no longer needed. This includes removing .npmrc, .nvmrc, .eslintrc.js, .prettierrc, SCSS base/_*, and several image files.

--- web/frontend/rl-astro-docs/astro.config.mjs
+++ web/frontend/rl-astro-docs/astro.config.mjs
@@ -1,19 +1,28 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from "astro-icon"
-import vercel from '@astrojs/vercel/serverless'
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
+// https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    }
-  }),
-  output: 'hybrid',
-})
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
