bc9f4fa03926cd0dff9b036a2835f0e7246671d9
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-03-29 12:31:35 +0000

    Enhance expense table layout and streamline component imports
    Refactored expense tables for consistent column widths and responsive design. Simplified component imports by removing redundant modules and cleaned up unnecessary declarations. Improved HTML structure for better readability and maintainability.

--- web/frontend/rl-angular-spa/src/app/components/pages/_properties/properties-dashboard/properties-dashboard.component.ts
+++ web/frontend/rl-angular-spa/src/app/components/pages/_properties/properties-dashboard/properties-dashboard.component.ts
@@ -1,5 +1,11 @@
-import {AsyncPipe, NgClass, NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
-import {ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatExpansionModule} from '@angular/material/expansion';
@@ -9,11 +15,9 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatTabsModule} from '@angular/material/tabs';
-import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {MenuItem} from 'primeng/api';
-import {Button} from 'primeng/button';
 import {ProgressSpinnerModule} from 'primeng/progressspinner';
-import {Ripple} from 'primeng/ripple';
 import {SplitButtonModule} from 'primeng/splitbutton';
 import {TabMenuModule} from 'primeng/tabmenu';
 import {TabViewModule} from 'primeng/tabview';
@@ -23,17 +27,16 @@ import {BehaviorSubject, filter, Subscription, take, tap} from 'rxjs';
 import {UserAuthenticationStore} from '../../../../+state/auth/user-auth.store';
 import {ExpensesStore} from '../../../../+state/ledger/expenses/expenses.store';
 import {PropertiesStore} from '../../../../+state/ledger/properties/properties.store';
-import {rebaseRoutePath, rebaseRoutePathAsString, RoutePath} from '../../../../app.routes';
+import {
+  rebaseRoutePath,
+  rebaseRoutePathAsString,
+  RoutePath,
+} from '../../../../app.routes';
 import {UtilService} from '../../../../services/util/util.service';
 import {RouterUtils} from '../../../../util/router/Router.utils';
-import {H1DarkModeComponent} from '../../../lib/_dark-mode/h1-dark-mode/h1-dark-mode.component';
-import {PDarkModeComponent} from '../../../lib/_dark-mode/p-dark-mode/p-dark-mode.component';
 import {CustomSplitsTabComponent} from '../../../lib/_expenses/custom-splits-tab/custom-splits-tab.component';
-import {ExpenseActionMenuComponent} from '../../../lib/_expenses/expense-action-menu/expense-action-menu.component';
 import {ExpenseTabComponent} from '../../../lib/_expenses/expense-tab/expense-tab.component';
-import {
-  PropertyDashboardPeopleTabComponent,
-} from '../../../lib/_properties/property-dashboard-people-tab/property-dashboard-people-tab.component';
+import {PropertyDashboardPeopleTabComponent} from '../../../lib/_properties/property-dashboard-people-tab/property-dashboard-people-tab.component';
 import {ReportsTabComponent} from '../../../lib/_properties/reports-tab/reports-tab.component';
 
 @Component({
@@ -42,10 +45,7 @@ import {ReportsTabComponent} from '../../../lib/_properties/reports-tab/reports-
   imports: [
     MatProgressSpinnerModule,
     FormsModule,
-    NgOptimizedImage,
     ReactiveFormsModule,
-    RouterLink,
-    AsyncPipe,
     NgIf,
     MatProgressBarModule,
     MatExpansionModule,
@@ -54,20 +54,15 @@ import {ReportsTabComponent} from '../../../lib/_properties/reports-tab/reports-
     MatListModule,
     MatMenuModule,
     MatButtonModule,
-    ExpenseActionMenuComponent,
     ExpenseTabComponent,
     ExpenseTabComponent,
     TabViewModule,
     ReportsTabComponent,
     ToolbarModule,
-    Button,
     SplitButtonModule,
     PropertyDashboardPeopleTabComponent,
     NgClass,
     TabMenuModule,
-    Ripple,
-    PDarkModeComponent,
-    H1DarkModeComponent,
     ProgressSpinnerModule,
     CustomSplitsTabComponent,
     NgForOf,
@@ -124,31 +119,31 @@ export class PropertiesDashboardComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.routeParamsSubscription = this.route.params
-      .pipe(
-        filter((params) => !!params['propertyId']),
-        tap((params) => {
-          this.propertyId = params['propertyId'];
-          this.propertiesStore.loadPropertyById(this.propertyId);
-        }),
-      )
-      .subscribe();
+        .pipe(
+            filter((params) => !!params['propertyId']),
+            tap((params) => {
+              this.propertyId = params['propertyId'];
+              this.propertiesStore.loadPropertyById(this.propertyId);
+            }),
+        )
+        .subscribe();
     this.routeQueryParamsSubscription = this.route.queryParams
-      .pipe(
-        filter((queryParams) => !!queryParams[this.TAB_QUERY_PARAM_KEY]),
-        tap((queryParams) => {
-          const tabQueryParam = queryParams[this.TAB_QUERY_PARAM_KEY];
-          if (tabQueryParam === 'expenses') {
-            this.activeItem = this.items?.[this.EXPENSE_TAB_INDEX];
-          } else if (tabQueryParam === 'custom-splits') {
-            this.activeItem = this.items?.[this.CUSTOM_SPLITS_TAB_INDEX];
-          } else if (tabQueryParam === 'reports') {
-            this.activeItem = this.items?.[this.REPORTS_TAB_INDEX];
-          } else if (tabQueryParam === 'people') {
-            this.activeItem = this.items?.[this.PEOPLE_TAB_INDEX];
-          }
-        }),
-      )
-      .subscribe();
+        .pipe(
+            filter((queryParams) => !!queryParams[this.TAB_QUERY_PARAM_KEY]),
+            tap((queryParams) => {
+              const tabQueryParam = queryParams[this.TAB_QUERY_PARAM_KEY];
+              if (tabQueryParam === 'expenses') {
+                this.activeItem = this.items?.[this.EXPENSE_TAB_INDEX];
+              } else if (tabQueryParam === 'custom-splits') {
+                this.activeItem = this.items?.[this.CUSTOM_SPLITS_TAB_INDEX];
+              } else if (tabQueryParam === 'reports') {
+                this.activeItem = this.items?.[this.REPORTS_TAB_INDEX];
+              } else if (tabQueryParam === 'people') {
+                this.activeItem = this.items?.[this.PEOPLE_TAB_INDEX];
+              }
+            }),
+        )
+        .subscribe();
   }
 
   ngOnDestroy() {
@@ -159,10 +154,10 @@ export class PropertiesDashboardComponent implements OnInit, OnDestroy {
   loadExpensesForCurrentMonth() {
     this.expensesStore.loadExpensesForProperty(this.propertyId, {
       startDate: this.utilService.formatDate(
-        this.utilService.getFirstDayOfMonthForDate(new Date()),
+          this.utilService.getFirstDayOfMonthForDate(new Date()),
       ),
       endDate: this.utilService.formatDate(
-        this.utilService.getLastDayOfMonthForDate(new Date()),
+          this.utilService.getLastDayOfMonthForDate(new Date()),
       ),
     });
   }
@@ -170,10 +165,10 @@ export class PropertiesDashboardComponent implements OnInit, OnDestroy {
   loadExpensesForSelectedMonthAndYear($event: Date) {
     this.expensesStore.loadExpensesForProperty(this.propertyId, {
       startDate: this.utilService.formatDate(
-        this.utilService.getFirstDayOfMonthForDate($event),
+          this.utilService.getFirstDayOfMonthForDate($event),
       ),
       endDate: this.utilService.formatDate(
-        this.utilService.getLastDayOfMonthForDate($event),
+          this.utilService.getLastDayOfMonthForDate($event),
       ),
     });
   }
@@ -193,65 +188,65 @@ export class PropertiesDashboardComponent implements OnInit, OnDestroy {
       tabQueryParam = 'people';
     }
     this.router
-      .navigate([], {
-        relativeTo: this.route,
-        queryParams: {tab: tabQueryParam},
-      })
-      .catch(RouterUtils.navigateCatchErrorCallback);
+        .navigate([], {
+          relativeTo: this.route,
+          queryParams: {tab: tabQueryParam},
+        })
+        .catch(RouterUtils.navigateCatchErrorCallback);
     this.isInitialLoad.asObservable().pipe(
-      take(1),
-      tap((isInitialLoad) => {
-        if (isInitialLoad) {
-          this.isInitialLoad.next(false);
-          return;
-        }
-        if ($event.label === this.items?.[this.EXPENSE_TAB_INDEX]?.label) {
-          this.router
-            .navigate([], {
-              relativeTo: this.route,
-              queryParams: {tab: 'expenses'},
-            })
-            .catch(RouterUtils.navigateCatchErrorCallback);
-          this.loadExpensesForCurrentMonth();
-        } else if (
-          $event.label === this.items?.[this.CUSTOM_SPLITS_TAB_INDEX]?.label
-        ) {
-          this.router
-            .navigate([], {
-              relativeTo: this.route,
-              queryParams: {tab: 'custom-splits'},
-            })
-            .catch(RouterUtils.navigateCatchErrorCallback);
-        } else if (
-          $event.label === this.items?.[this.REPORTS_TAB_INDEX]?.label
-        ) {
-          this.router
-            .navigate([], {
-              relativeTo: this.route,
-              queryParams: {tab: 'reports'},
-            })
-            .catch(RouterUtils.navigateCatchErrorCallback);
-          this.loadExpensesForCurrentMonth();
-          this.resetReportsTabComponent();
-        } else if (
-          $event.label === this.items?.[this.PEOPLE_TAB_INDEX]?.label
-        ) {
-          this.router
-            .navigate([], {
-              relativeTo: this.route,
-              queryParams: {tab: 'people'},
-            })
-            .catch(RouterUtils.navigateCatchErrorCallback);
-        }
-      }),
+        take(1),
+        tap((isInitialLoad) => {
+          if (isInitialLoad) {
+            this.isInitialLoad.next(false);
+            return;
+          }
+          if ($event.label === this.items?.[this.EXPENSE_TAB_INDEX]?.label) {
+            this.router
+                .navigate([], {
+                  relativeTo: this.route,
+                  queryParams: {tab: 'expenses'},
+                })
+                .catch(RouterUtils.navigateCatchErrorCallback);
+            this.loadExpensesForCurrentMonth();
+          } else if (
+            $event.label === this.items?.[this.CUSTOM_SPLITS_TAB_INDEX]?.label
+          ) {
+            this.router
+                .navigate([], {
+                  relativeTo: this.route,
+                  queryParams: {tab: 'custom-splits'},
+                })
+                .catch(RouterUtils.navigateCatchErrorCallback);
+          } else if (
+            $event.label === this.items?.[this.REPORTS_TAB_INDEX]?.label
+          ) {
+            this.router
+                .navigate([], {
+                  relativeTo: this.route,
+                  queryParams: {tab: 'reports'},
+                })
+                .catch(RouterUtils.navigateCatchErrorCallback);
+            this.loadExpensesForCurrentMonth();
+            this.resetReportsTabComponent();
+          } else if (
+            $event.label === this.items?.[this.PEOPLE_TAB_INDEX]?.label
+          ) {
+            this.router
+                .navigate([], {
+                  relativeTo: this.route,
+                  queryParams: {tab: 'people'},
+                })
+                .catch(RouterUtils.navigateCatchErrorCallback);
+          }
+        }),
     );
   }
 
   getPropertyTier() {
     return this.propertiesStore.propertyById().creatorPaymentStatus.status ===
-      'UNPAID'
-      ? 'Basic Tier'
-      : 'Premium Tier';
+      'UNPAID' ?
+      'Basic Tier' :
+      'Premium Tier';
   }
 
   private resetReportsTabComponent() {
