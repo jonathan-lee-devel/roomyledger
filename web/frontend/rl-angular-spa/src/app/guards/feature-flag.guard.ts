a989ea789ad11162b8c4f2fd89f94b1d9b5048e7
jonathanlee <jonathan.lee.devel@gmail.com>
2025-10-04 06:20:55 +0100

    Switch feature flag guard to use Zenigo FlagService

--- web/frontend/rl-angular-spa/src/app/guards/feature-flag.guard.ts
+++ web/frontend/rl-angular-spa/src/app/guards/feature-flag.guard.ts
@@ -1,17 +1,17 @@
 import {inject} from '@angular/core';
 import {CanActivateFn} from '@angular/router';
+import {FlagService} from 'zenigo-client-sdk';
 
 import {UserAuthenticationStore} from '../+state/auth/user-auth.store';
-import {FeatureFlagsStore} from '../+state/feature-flags/feature-flags.store';
 import {FeatureFlagEnum} from '../enums/FeatureFlag.enum';
 
 export const featureFlagGuard: CanActivateFn = (route) => {
   const userAuthenticationStore = inject(UserAuthenticationStore);
-  const featureFlagsStore = inject(FeatureFlagsStore);
+  const flagService = inject(FlagService);
   const featureFlagEnum = route.data['feature'] as FeatureFlagEnum;
-  const isActive = featureFlagsStore.featureFlags()
-      .find((featureFlag) => featureFlag.featureName === featureFlagEnum)
-      ?.isActive;
+  const isActive = flagService.flags()
+      .find((featureFlag) => featureFlag.key === featureFlagEnum)
+      ?.isEnabled ?? false;
   if (!isActive) {
     userAuthenticationStore.onLoginError(new Error('That feature has been disabled'));
     return false;
